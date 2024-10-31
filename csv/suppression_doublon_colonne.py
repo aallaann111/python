57f5c28593409da98d7f2d941a87b8d44fc1f2ce
alan TERRIER <alan.terrier@etu.umontpellier.fr>
2024-10-31 11:11:02 +0100

    duplicate colonne

--- csv/suppression_doublon_colonne.py
+++ csv/suppression_doublon_colonne.py
@@ -17,13 +17,16 @@ duplicate_column = input("Entrez le nom de la colonne contenant les doublons : "
 if reference_column not in data.columns or duplicate_column not in data.columns:
     print("Erreur : Une des colonnes spécifiées n'existe pas dans le fichier.")
     exit()
-    
+
 output_folder = "resultat"
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-deduplicated_data = data.drop_duplicates(subset=[reference_column, duplicate_column])
+data[duplicate_column] = data.apply(
+    lambda row: '' if row[duplicate_column] == row[reference_column] else row[duplicate_column],
+    axis=1
+)
 
-output_file = output_folder + "/deduplicated_colonne_" + csv_file
-deduplicated_data.to_csv(output_file, index=False)
+output_file = os.path.join(output_folder, "deduplicated_colonne_" + csv_file)
+data.to_csv(output_file, index=False)
 print(f"Fichier sans doublons '{output_file}' créé avec succès.")
